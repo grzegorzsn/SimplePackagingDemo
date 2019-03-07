42ae0f66bec202f7c99251aff99cc7f54ae31a2a
Grzegorz Skupien <grzegorz.skupien4@orange.com>
2019-03-07 08:56:46 +0100

    Add entry_point

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="superscrapper",
-    version="0.1",
+    version="0.2",
     author="Grzegorz Skupien",
     author_email="grzegorz.skupien4@orange.com, grzegorz@skupien.net",
     packages=find_packages(),
@@ -10,5 +10,10 @@ setup(
     classifiers=[
         'Operating System :: POSIX',
         'Programming Language :: Python :: 3.6'
-    ]
+    ],
+    entry_points={
+        'console_scripts': [
+            'scrap = superscrapper.scrapper:main'
+        ]
+    },
 )
--- superscrapper/scrapper.py
+++ superscrapper/scrapper.py
@@ -1,6 +1,16 @@
+import sys
+
 import requests
 
 
 def check_response(url):
     response = requests.get(url)
     print(response.status_code)
+
+
+def main():
+    check_response(sys.argv[1])
+
+
+if __name__ == '__main__':
+    main()
