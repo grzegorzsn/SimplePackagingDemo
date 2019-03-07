8d428cf58452f4721e938854bd3a50f0fceb6988
Grzegorz Skupien <grzegorz.skupien4@orange.com>
2019-03-07 09:03:21 +0100

    Unify requirements

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 from setuptools import find_packages, setup
 
+
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+
+def get_requirements():
+    return parse_requirements("requirements.txt")
+
+
 setup(
     name="superscrapper",
-    version="0.2",
+    version="0.3",
     author="Grzegorz Skupien",
     author_email="grzegorz.skupien4@orange.com, grzegorz@skupien.net",
     packages=find_packages(),
-    install_requires=["requests>=2"],
+    install_requires=get_requirements(),
     classifiers=[
         'Operating System :: POSIX',
         'Programming Language :: Python :: 3.6'
